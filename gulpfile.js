b989fd2ca0dd6a001b7f5cdca3148c45d2000420
Paulo Munoz <pvanet@gmail.com>
2015-11-30 22:15:08 +0000

    Adding live-reload, finishing bootstrap cleanup and adding Susy

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,15 @@ var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var shell = require('gulp-shell')
+var livereload = require('gulp-livereload');
 
 gulp.task('js', function(){
   browserify('./assets/js/app.js')
     .transform("babelify", {presets: ["react"]})
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('public/js/'));
+    .pipe(gulp.dest('public/js/'))
+    .pipe(livereload());
 });
 
 
@@ -24,10 +26,12 @@ gulp.task('sass', function(){
     .pipe(sass().on('error',sass.logError))
     .pipe(autoprefixer({ browsers: ['ie 9', 'last 2 versions'], cascade: false }))
     .pipe(gulp.dest('public/css'))
+    .pipe(livereload());
 });
 
 
 gulp.task('watch', function() {
+  livereload.listen();
   gulp.watch("assets/js/**/*.js", ["js"]);
   gulp.watch("assets/scss/**/*.scss", ["sass"])
 })
